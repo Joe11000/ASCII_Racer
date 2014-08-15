58684cc6a84991c5d765751aa48169ef9ba0e8e9
Joe Noonan <joenoonan27@gmail.com>
2014-08-14 23:16:06 -0500

    Sublime auto fixed spacing at end of lines

--- ruby_racer.rb
+++ ruby_racer.rb
@@ -1,12 +1,12 @@
-# race car application 
+# race car application
 # input : array of racer names
 # output: no valuable output (besides pretty race and congradulatory comments to winner)
 
 require_relative 'racer_utils'
- 
+
 class RubyRacer
   attr_reader :players, :length
- 
+
   def initialize(players=2, length = 30)
     @players = players
     @player_colors = ["\033[92m","\033[91m","\033[96m", "\033[94m","\033[95m", "\033[93m"] # green, red, light-blue, blue, pink, yellow
@@ -15,11 +15,11 @@ class RubyRacer
     @racers_hash = {}
     players.each {|player| @racers_hash[player] = 1 }
   end
-  
-  # Returns +true+ if one of the players has reached 
+
+  # Returns +true+ if one of the players has reached
   # the finish line, +false+ otherwise
   def finished?(player)
-    @racers_hash[player] >= length-1 
+    @racers_hash[player] >= length-1
   end
 
   # Rolls the dice and advances +player+ accordingly
@@ -32,7 +32,7 @@ class RubyRacer
       @racers_hash[player] += single_roll
     end
   end
-  
+
   # Prints the current game board
   # The board should have the same dimensions each time
   # and you should use the "reputs" helper to print over
@@ -40,20 +40,20 @@ class RubyRacer
   def print_board
     color_end = "\033[0m"
     loop_counter = 0
-    @players.each do |p| 
-      print " | " * (@racers_hash[p] - 1) + @player_colors[loop_counter % @player_colors.size] 
+    @players.each do |p|
+      print " | " * (@racers_hash[p] - 1) + @player_colors[loop_counter % @player_colors.size]
       print " #{@players[loop_counter][0]} "
-      print color_end 
+      print color_end
       print  " | " * (@length - (@racers_hash[p]) - 1)
       loop_counter += 1
       puts
     end
   end
-end 
+end
 
 def startRace(names_array)
   game = RubyRacer.new(names_array)
- 
+
   clear_screen! # This clears the screen, so the fun can begin
 
   move_to_home! # This moves the cursor back to the upper-left of the screen
@@ -61,18 +61,17 @@ def startRace(names_array)
 
   names_array.cycle do |player|
     move_to_home! # This moves the cursor back to the upper-left of the screen
-      
+
     game.advance_player!(player) # We print the board first so we see the initial, starting board
     game.print_board
-    if game.finished?(player) 
+    if game.finished?(player)
       puts "#{player.capitalize!} WON!!!!!"
       puts "*Slap on the butt.* Thanks for playing."
       break
     end
-    
+
     names_array.size >= 10 ? sleep(0.05) : sleep(0.1) # if number if racers is > 10 then sleep less time
   end
 end
 
-
-startRace(('a'..'p').to_a)
+startRace(('a'..'z').to_a)
